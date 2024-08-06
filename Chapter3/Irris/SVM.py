2601618852fb8582b371bd1327ce737fa333240a
Hand_On_ML_Project_withOpenCV <dungvu24042k3@gmail.com>
2024-08-06 08:58:49 +0700

    baitap

--- Chapter3/Irris/SVM.py
+++ Chapter3/Irris/SVM.py
@@ -13,38 +13,28 @@ iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-# Chọn hai thuộc tính để dễ dàng trực quan hóa
-X = X[:, [2, 3]]  # Petal length và Petal width
-
-# Chia dữ liệu thành tập huấn luyện và tập kiểm tra
+X = X[:, [2, 3]]  
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Tiêu chuẩn hóa dữ liệu
 sc = StandardScaler()
 X_train_std = sc.fit_transform(X_train)
 X_test_std = sc.transform(X_test)
 
-# Kết hợp dữ liệu huấn luyện và kiểm tra để vẽ vùng quyết định
 X_combined_std = np.vstack((X_train_std, X_test_std))
 y_combined = np.hstack((y_train, y_test))
 
-# Khởi tạo và huấn luyện mô hình SVM
 svm = SVC(kernel='linear', C=1.0, random_state=1)
 svm.fit(X_train_std, y_train)
 
-# Định nghĩa hàm để vẽ các vùng quyết định
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
-    # Đặt giới hạn cho các trục
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 
-    # Tạo lưới các điểm để dự đoán các vùng quyết định
     xx, yy = np.meshgrid(np.arange(x_min, x_max, resolution),
                          np.arange(y_min, y_max, resolution))
     Z = classifier.predict(np.array([xx.ravel(), yy.ravel()]).T)
     Z = Z.reshape(xx.shape)
 
-    # Vẽ các vùng quyết định
     plt.contourf(xx, yy, Z, alpha=0.3, cmap=ListedColormap(('red', 'green', 'blue')))
     plt.scatter(X[:, 0], X[:, 1], c=y, edgecolor='k', marker='o', cmap=ListedColormap(('red', 'green', 'blue')))
 
